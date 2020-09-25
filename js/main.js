eebb63fc706eb34e1bf30ae098dcba05585d4325
DimaSilver <dimka7600@ukr.net>
2020-09-25 19:23:09 +0300

    Застилизовал сэкшен reviews

--- js/main.js
+++ js/main.js
@@ -8,4 +8,8 @@ $(function(){
         prevArrow: '<button type="button" class="individual-order__item-design__slider-prev individual-order__item-design__slider-arrows"><img src="images/arrow-left__slider.svg" alt=""></button>',
         nextArrow: '<button type="button" class="individual-order__item-design__slider-next individual-order__item-design__slider-arrows"><img src="images/arrow-right__slider.svg" alt=""></button>'
     });
+	$('.reviews__slider').slick({
+        prevArrow: '<button type="button" class="reviews__slider-prev reviews__slider-arrows"><img src="images/arrow-left__slider.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="reviews__slider-next reviews__slider-arrows"><img src="images/arrow-right__slider.svg" alt=""></button>'
+    });
 });
