a95b8376ffd4384abcca504e06d18f42a1ee03cd
DimaSilver <dimka7600@ukr.net>
2020-09-25 18:43:43 +0300

    Застилизовал сэкшен individual-order

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function(){
-	
+	$('.individual-order__slider').slick({
+        prevArrow: '<button type="button" class="individual-order__slider-prev individual-order__slider-arrows"><img src="images/arrow-left__slider.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="individual-order__slider-next individual-order__slider-arrows"><img src="images/arrow-right__slider.svg" alt=""></button>'
+    });
+	$('.individual-order__item-design__slider').slick({
+        slidesToShow: 2,
+        prevArrow: '<button type="button" class="individual-order__item-design__slider-prev individual-order__item-design__slider-arrows"><img src="images/arrow-left__slider.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="individual-order__item-design__slider-next individual-order__item-design__slider-arrows"><img src="images/arrow-right__slider.svg" alt=""></button>'
+    });
 });
