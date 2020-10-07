3f58a941cdabfdd6b18612c9d76c072fac477063
DimaSilver <dimka7600@ukr.net>
2020-10-07 16:41:55 +0300

    Адаптив под 768 пикселей

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 $(function(){
-	$('.individual-order__slider').slick({
+    $('.individual-order__slider').slick({
         prevArrow: '<button type="button" class="individual-order__slider-prev individual-order__slider-arrows"><img src="images/arrow-left__slider.svg" alt=""></button>',
         nextArrow: '<button type="button" class="individual-order__slider-next individual-order__slider-arrows"><img src="images/arrow-right__slider.svg" alt=""></button>'
     });
-	$('.individual-order__item-design__slider').slick({
+    $('.individual-order__item-design__slider').slick({
         slidesToShow: 2,
         prevArrow: '<button type="button" class="individual-order__item-design__slider-prev individual-order__item-design__slider-arrows"><img src="images/arrow-left__slider.svg" alt=""></button>',
         nextArrow: '<button type="button" class="individual-order__item-design__slider-next individual-order__item-design__slider-arrows"><img src="images/arrow-right__slider.svg" alt=""></button>'
     });
-	$('.reviews__slider').slick({
+    $('.reviews__slider').slick({
         prevArrow: '<button type="button" class="reviews__slider-prev reviews__slider-arrows"><img src="images/arrow-left__slider.svg" alt=""></button>',
         nextArrow: '<button type="button" class="reviews__slider-next reviews__slider-arrows"><img src="images/arrow-right__slider.svg" alt=""></button>'
+    });
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list-active');
     });
 });
